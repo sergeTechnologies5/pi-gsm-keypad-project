d0e8313e49e91abbc74171418887d44a9f55ea56
PHINEAS NTHIGA <njesh74@gmail.com>
2020-10-13 12:20:52 +0300

    f

--- flaskapp.py
+++ flaskapp.py
@@ -70,11 +70,11 @@ def sendMessage():
     message = content['message']
     number  = content['number']
     # Sending a message to a particular Number
-    port.write(b'AT+CMGS="'+number.encode()+'"'+b'\r\n')
+    port.write(b'AT+CMGS="+254702261679"'+b'\r\n')
     rcv = port.read(10)
     print (rcv)
     time.sleep(1)
-    port.write(b'{}'.format(message)+b'\r\n') # Message
+    port.write(b'Hello Ndege Technologies'+b'\r\n') # Message
     rcv = port.read(10)
     print (rcv)
     return "Message sent"
