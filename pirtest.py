eb6371f882d6944974fbebcfafbe63192aedceba
PHINEAS NTHIGA <njesh74@gmail.com>
2020-11-03 09:01:17 +0300

    G

--- pirtest.py
+++ pirtest.py
@@ -9,10 +9,10 @@ GPIO.setup(bazar, GPIO.OUT)         #LED output pin
 while True:
     i=GPIO.input(pir)
     if i==0:                 #When output from motion sensor is LOW
-        print "No intruders",i
+        print("No intruders",i)
         GPIO.output(bazar, 0)  #Turn OFF LED
         time.sleep(0.1)
     elif i==1:               #When output from motion sensor is HIGH
-        print "Intruder detected",i
+        print("Intruder detected",i)
         GPIO.output(bazar, 1)  #Turn ON LED
         time.sleep(0.1)
