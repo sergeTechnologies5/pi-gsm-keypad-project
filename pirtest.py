84268fa423c9682810f558771849d8c2adc6120c
PHINEAS NTHIGA <njesh74@gmail.com>
2020-11-03 09:07:02 +0300

    G

--- pirtest.py
+++ pirtest.py
@@ -6,13 +6,24 @@ pir = 23
 bazar = 24
 GPIO.setup(pir, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(bazar, GPIO.OUT)         #LED output pin
-while True:
-    i=GPIO.input(pir)
-    if i==0:                 #When output from motion sensor is LOW
-        print("No intruders",i)
-        GPIO.output(bazar, 0)  #Turn OFF LED
-        time.sleep(0.1)
-    elif i==1:               #When output from motion sensor is HIGH
-        print("Intruder detected",i)
-        GPIO.output(bazar, 1)  #Turn ON LED
-        time.sleep(0.1)
+
+class Pir :
+
+    def __init__(self):
+        p = Process(target=self.run, args=())
+        p.daemon = True                       # Daemonize it
+        p.start() 
+         
+    def listenKeypad(self):
+        while True:
+            i=GPIO.input(pir)
+            if i==0:                 #When output from motion sensor is LOW
+                print("No intruders",i)
+                GPIO.output(bazar, 0)  #Turn OFF LED
+                time.sleep(0.1)
+            elif i==1:               #When output from motion sensor is HIGH
+                print("Intruder detected",i)
+                GPIO.output(bazar, 1)  #Turn ON LED
+                time.sleep(0.1)
+
+pir = Pir()
