5814bd292e74d142e40998008bb883d2fb0968df
PHINEAS NTHIGA <njesh74@gmail.com>
2020-11-04 14:07:38 +0300

    G

--- pirtest.py
+++ pirtest.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 from multiprocessing import Process
+import requests
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -20,12 +21,19 @@ class Pir:
     def startDetectingMotion(self):
         while True:
             i = GPIO.input(pr)
+            sent = False
             if i == 0:  # When output from motion sensor is LOW
                 GPIO.output(bazar, 0)  # Turn OFF LED
                 time.sleep(0.1)
+                sent = False
             elif i == 1:  # When output from motion sensor is HIGH
                 GPIO.output(bazar, 1)  # Turn ON LED
                 time.sleep(0.1)
+                if not sent:
+                    # send
+                    data = {"message":"Someone is standing at the door of the vault","number":"254714195834"}
+                    r = requests.post("http://localhost:5000/send",data=data)
+                    sent = True
 
     def run(self):
         # This might take several minutes to complete
